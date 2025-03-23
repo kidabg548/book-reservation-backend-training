543fbecb938c54f4ef650c34f23d908a1a9352bc
kidabg548 <kidabg548@gmail.com>
2025-03-23 23:28:57 +0300

    fiexed app.js file erros

--- app.js
+++ app.js
@@ -30,5 +30,10 @@ app.use("/api/reservations", reservationRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'test') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+
+module.exports = app; 
